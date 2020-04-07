6e43f5a9a2a98286c6d03b6481d3da55bde06ebd
raziel <razieldev90@gmail.com>
2020-04-07 14:53:28 +0200

    add contact section & fix mobile design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+const toggle = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('show');
+  toggle.classList.toggle('active');
+});
+
 const questions = document.querySelectorAll('.faq__question');
 
 questions.forEach((question) => {
